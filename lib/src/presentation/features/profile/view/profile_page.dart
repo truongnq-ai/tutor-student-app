ec28cbcc9772d1769dfd598918a83cc349f4cd05
truongnq <truongnq.ai@gmail.com>
2025-12-25 02:03:52 +0700

    Update profile_page.dart

--- lib/src/presentation/features/profile/view/profile_page.dart
+++ lib/src/presentation/features/profile/view/profile_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../core/router/routes.dart';
+import '../../../core/widgets/text/typography.dart';
 
 class ProfilePage extends ConsumerWidget {
   const ProfilePage({super.key});
